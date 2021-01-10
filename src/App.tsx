e0dca1b6245f35ae72d8ceb54900f06ade627977
Alberto Chang <alberto.chang@aviaa.com>
2021-01-09 20:46:02 -0600

    Add wouter and initial routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { Router, Route } from 'wouter'
 
-import { RootState } from './store'
-import { setBreakpoint } from './store/actions/layoutsActions'
+import { HomeContainer } from './containers/HomeContainer'
 
-import logo from './logo.svg'
-import './App.scss'
-
-function App() {
-  const layouts = useSelector((state: RootState) => state.layouts)
-  const dispatch = useDispatch()
-
-  const handleChangeBreakpoint = (e: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(setBreakpoint('desktop'))
-  }
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <p>Current Breakpoint: {layouts.breakpoint}</p>
-        <button onClick={handleChangeBreakpoint}>Set Breakpoint</button>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" component={HomeContainer} />
+    </Router>
   )
 }
 
---
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { RootState } from '../../store'
+import { setBreakpoint } from '../../store/actions/layoutsActions'
+
+import logo from '../../images/logo.svg'
+import './HomeContainer.scss'
+
+export const HomeContainer = () => {
+  const layouts = useSelector((state: RootState) => state.layouts)
+  const dispatch = useDispatch()
+
+  const handleChangeBreakpoint = (e: React.MouseEvent<HTMLButtonElement>) => {
+    dispatch(setBreakpoint('desktop'))
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        <p>Current Breakpoint: {layouts.breakpoint}</p>
+        <button onClick={handleChangeBreakpoint}>Set Breakpoint</button>
+      </header>
+    </div>
+  )
+}
